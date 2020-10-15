5e209643e156d1bc2270ad8da6a1a839ae24545d
Favalcodes <favourvalg@gmail.com>
2020-10-15 22:16:42 +0100

    Login page done

---
+++ js/login.js
@@ -0,0 +1,10 @@
+$('.toggle-password').on('click', function() {
+    $(this).toggleClass('fa-eye fa-eye-slash');
+    let input = $($(this).attr('toggle'));
+    if (input.attr('type') == 'password') {
+      input.attr('type', 'text');
+    }
+    else {
+      input.attr('type', 'password');
+    }
+  });
